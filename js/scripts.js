f19301406ebd4d50b8ea55ccbbe25d638ab51d9a
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-21 19:33:28 -0300

    Tabuleiro OK

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
 (function(){
-    
+    var tiles = [];
+
+    function init(){
+        for(var i = 1; i < 9; i++){
+            var tile = document.querySelector("#n"+i);
+            tile.style.background = "url('images/n"+i+".png')";
+            tiles.push(tile);
+        }
+        tiles.push(null);
+        render();
+    }
+
+    function render(){
+        for(var i in tiles){
+            var tile = tiles[i];
+            if(tile){
+                tile.style.left = (i%3) * 100 + 5 + "px";
+                if(i < 3){
+                    tile.style.top = "5px";
+                }else
+                if(i < 6){
+                    tile.style.top = "105px";
+                }else {
+                    tile.style.top = "205px";
+                }
+            }
+        }
+    }
+
+    init();
 }());
