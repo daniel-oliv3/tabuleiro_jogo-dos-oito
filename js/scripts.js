5195375a202d23c129639738c7801850b918c257
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-21 19:53:24 -0300

    Start-Screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 (function(){
-    var tiles = [];
+    var tiles = []; //arrays que arazenam o estado do jogo e o de vitria, respectivamente
 
+    //função que inicializa os elementos do jogo
     function init(){
-        for(var i = 1; i < 9; i++){
+        for(var i = 1; i < 9; i++){ //varre os elementos 'btn' adicionando a imagem e inserindo os elementos no array
             var tile = document.querySelector("#n"+i);
             tile.style.background = "url('images/n"+i+".png')";
             tiles.push(tile);
         }
-        tiles.push(null);
+        tiles.push(null); //completa o array com um espaço nulo e o copia para a resposta, depois renderiza o tabuleiro
         render();
     }
 
+    //ajusta a exibição do tabuleiro em função do array tiles
     function render(){
         for(var i in tiles){
             var tile = tiles[i];
